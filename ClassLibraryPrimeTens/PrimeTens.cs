412d36932f6dae568981037239856ef084892554
UsikovKonstantin <99312736+UsikovKonstantin@users.noreply.github.com>
2022-09-03 13:54:02 +0300

    Перебор до корня

--- ClassLibraryPrimeTens/PrimeTens.cs
+++ ClassLibraryPrimeTens/PrimeTens.cs
@@ -45,5 +45,41 @@
             return arr;
         }
         #endregion
+
+        #region Перебор до корня
+        /// <summary>
+        /// Возвращает массив булевых значений, указывающих, является ли число простым.
+        /// Использует алгоритм поиска простых чисел с проверкой делителей до корня.
+        /// Пример: prime[2] = true, prime[3] = true, prime[4] = false, prime[5] = true.
+        /// </summary>
+        /// <param name="n"> верхняя граница массива </param>
+        /// <returns> массив булевых значений, указывающих, является ли число простым </returns>
+        public static bool[] GetPrimeNumbersSqrt(int n)
+        {
+            bool[] prime = new bool[n + 1];
+            for (int i = 2; i <= n; i++)
+            {
+                prime[i] = IsPrime(i);
+            }
+            return prime;
+        }
+
+        /// <summary>
+        /// Возвращает булевое значение, является ли число простым.
+        /// </summary>
+        /// <param name="x"> число для проверки </param>
+        /// <returns> true - если число простое, false - если составное </returns>
+        public static bool IsPrime(int x)
+        {
+            for (int i = 2; i <= (int)Math.Sqrt(x); i++)
+            {
+                if (x % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        #endregion
     }
 }
