c5e6d3ed6b04223a0109bad976222ec89ca21381
Nydus Born <nydusborn@outlook.com>
2022-09-04 10:44:25 +0300

    Убрал Await в консоли

--- ConsoleAppPrimeTens/Program.cs
+++ ConsoleAppPrimeTens/Program.cs
@@ -6,14 +6,14 @@ CancellationToken ct = new();
 (int, int, int, int) result;
 Stopwatch sw = Stopwatch.StartNew();
 bool[] nums = PrimeTens.GetPrimeNumbersEratosthenes(n, ct);
-result = await PrimeTens.GetMinMaxTens(nums, ct);
+result = PrimeTens.GetMinMaxTens(nums, ct);
 sw.Stop();
 Console.WriteLine(sw.ElapsedMilliseconds);
 Console.WriteLine($"{result.Item1} {result.Item2} {result.Item3} {result.Item4}");
 
 sw.Restart();
 bool[] nums2 = PrimeTens.GetPrimeNumbersSqrt(n, ct);
-result = await PrimeTens.GetMinMaxTens(nums2, ct);
+result = PrimeTens.GetMinMaxTens(nums2, ct);
 sw.Stop();
 Console.WriteLine(sw.ElapsedMilliseconds);
 Console.WriteLine($"{result.Item1} {result.Item2} {result.Item3} {result.Item4}");
